6a2dc41f2e665ad4e123555f5344751a128b14e3
premtumu3 <premtumu3@gmail.com>
2025-09-06 15:20:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -99,13 +99,13 @@ cidr_blocks = ["0.0.0.0/0"]
 }
 ingress {
 from_port = 80
-t0_port = 80
+to_port = 80
 protocol = "tcp"
 cidr_blocks = ["0.0.0.0/0"]
 }
 engress {
 from_port = 0
-t0_port = 0
+to_port = 0
 protocol = "-1"
 cidr_blocks = ["0.0.0.0/0"]
 }
